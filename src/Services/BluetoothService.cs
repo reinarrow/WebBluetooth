06e40dd96667f19b9d13f79b17be260291b639e1
Lidia <lidia.parrilla@idener.es>
2021-04-30 22:34:40 +0200

    feat(general): Project structure customisation #2

---
+++ src/Services/BluetoothService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace WebBluetooth.Services
+{
+    public class BluetothService : IBluetothService
+    {
+    }
+}
---
+++ src/Services/IWeatherForecastService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebBluetooth.Data.Models;
+
+namespace WebBluetooth.Services
+{
+    public interface IWeatherForecastService
+    {
+        Task<List<WeatherForecast>> CalculateForecast(DateTime startDate);
+    }
+}
